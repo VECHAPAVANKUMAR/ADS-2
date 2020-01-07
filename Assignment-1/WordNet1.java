e7218ce5fcd81718018b1ef3b445717dcd60fe48
VECHAPAVANKUMAR <vechapavan1997@msitprogram.net>
2020-01-07 09:05:14 +0530

    First

--- Assignment-1/WordNet1.java
+++ Assignment-1/WordNet1.java
@@ -11,15 +11,17 @@ import java.util.List;
 public class WordNet1 {
     List<Synset> synsetObjList;
     List<Integer> hypMapObj;
-    private HashMap<String, Synset> hashMapObj;
-    private HashMap<Integer, List<String>> hypernymsHashMapObj;
+    private final HashMap<String, Synset> hashMapObj;
+    private final HashMap<Integer, List<String>> hypernymsHashMapObj;
+
     public WordNet1() {
         synsetObjList = new ArrayList<>();
         hashMapObj = new HashMap<>();
         hypernymsHashMapObj = new HashMap<>();
         hypMapObj = new ArrayList<>();
     }
-    class Synset { 
+
+    class Synset {
         private String id;
         private String[] synsetArr;
         private String glossary;
@@ -28,7 +30,7 @@ public class WordNet1 {
 
         }
 
-        public Synset(String synsetId, String[] ssArr, String glsy) {
+        public Synset(final String synsetId, final String[] ssArr, final String glsy) {
             this.id = synsetId;
             this.synsetArr = ssArr;
             this.glossary = glsy;
@@ -38,7 +40,7 @@ public class WordNet1 {
             return id;
         }
 
-        public void setId(String id) {
+        public void setId(final String id) {
             this.id = id;
         }
 
@@ -46,7 +48,7 @@ public class WordNet1 {
             return synsetArr;
         }
 
-        public void setSynsetArr(String[] synsetArr) {
+        public void setSynsetArr(final String[] synsetArr) {
             this.synsetArr = synsetArr;
         }
 
@@ -54,52 +56,53 @@ public class WordNet1 {
             return glossary;
         }
 
-        public void setGlossary(String glossary) {
+        public void setGlossary(final String glossary) {
             this.glossary = glossary;
         }
 
         @Override
         public String toString() {
-            return "Synset [" + " id = " + id + " synsetArr = " + Arrays.toString(synsetArr) + " glossary = " + glossary + " ]";
+            return "Synset [" + " id = " + id + " synsetArr = " + Arrays.toString(synsetArr) + " glossary = " + glossary
+                    + " ]";
         }
-    } 
+    }
 
-        private void parseSynsets(String fileName) throws IOException {
-            Path path = Paths.get(fileName);
-            List<String> allLines = Files.readAllLines(path);
-            for (int i = 0; i < allLines.size(); i++) {
-                String[] arr = allLines.get(i).split(",");
-                Synset obj = new Synset(arr[0], arr[1].split(" "), arr[2]);
-                hashMapObj.put(arr[0], obj);
-                synsetObjList.add(obj);
-            }
+    private void parseSynsets(final String fileName) throws IOException {
+        final Path path = Paths.get(fileName);
+        final List<String> allLines = Files.readAllLines(path);
+        for (int i = 0; i < allLines.size(); i++) {
+            final String[] arr = allLines.get(i).split(",");
+            final Synset obj = new Synset(arr[0], arr[1].split(" "), arr[2]);
+            hashMapObj.put(arr[0], obj);
+            synsetObjList.add(obj);
         }
+    }
 
-        private void parseHypernyms(String fileName) throws IOException {
-            Path path = Paths.get(fileName);
-            List<String> allLines = Files.readAllLines(path);
-            String[] arr;
-            for (int i = 0; i < allLines.size(); i++) {
-                if (allLines.get(i).contains(",")) {
-                    arr = allLines.get(i).split(",",2);
-                    hypernymsHashMapObj.put(Integer.parseInt(arr[0]), Arrays.asList(arr[1]));
-                    hypMapObj.add(Integer.parseInt(arr[0]));
-                    for (String key : arr[1].split(",")) {
-                        hypernymsHashMapObj.put(Integer.parseInt(key), null);
-                        hypMapObj.add(Integer.parseInt(key));
-                    }
-                } else {
-                    arr = allLines.get(i).split(" ");
-                    hypernymsHashMapObj.put(Integer.parseInt(arr[0]), null);
-                    hypMapObj.add(Integer.parseInt(arr[0]));
+    private void parseHypernyms(final String fileName) throws IOException {
+        final Path path = Paths.get(fileName);
+        final List<String> allLines = Files.readAllLines(path);
+        String[] arr;
+        for (int i = 0; i < allLines.size(); i++) {
+            if (allLines.get(i).contains(",")) {
+                arr = allLines.get(i).split(",", 2);
+                hypernymsHashMapObj.put(Integer.parseInt(arr[0]), Arrays.asList(arr[1]));
+                hypMapObj.add(Integer.parseInt(arr[0]));
+                for (final String key : arr[1].split(",")) {
+                    hypernymsHashMapObj.put(Integer.parseInt(key), null);
+                    hypMapObj.add(Integer.parseInt(key));
                 }
+            } else {
+                arr = allLines.get(i).split(" ");
+                hypernymsHashMapObj.put(Integer.parseInt(arr[0]), null);
+                hypMapObj.add(Integer.parseInt(arr[0]));
             }
         }
+    }
 
-    public static void main(String[] args) throws IOException {
-    WordNet1 wordNet1Obj  = new WordNet1();
-    File folder = new File("G:\\Github\\ADS-2\\WORDNET");
-    File[] listOfFiles = folder.listFiles();
+    public static void main(final String[] args) throws IOException {
+        final WordNet1 wordNet1Obj = new WordNet1();
+        final File folder = new File("G:\\Github\\ADS-2\\WORDNET");
+        final File[] listOfFiles = folder.listFiles();
     
     for (int i = 0; i < listOfFiles.length; i++) {
         if (listOfFiles[i].isFile() && listOfFiles[i].getName().startsWith("synsets")) {
